00268826366b1633c23d8d8f2774e1db61aecb69
Ritesh Kriplani <kriplanira@rknec.edu>
2025-06-26 02:13:28 +0530

    solved error

--- client/src/socket/socket.tsx
+++ client/src/socket/socket.tsx
@@ -1,5 +1,8 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://walmart-xjjd.onrender.com");
+const socket = io("https://walmart-xjjd.onrender.com", {
+  withCredentials: true,
+  transports: ['websocket'], // Optional but helps on Render
+});
 
 export default socket;
